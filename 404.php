d687ab7d44c9d8fdbfc1e44d54bc7e05785383a1
Marielad <marieladorta@gmail.com>
2019-07-12 10:22:45 +0100

    404 headerless

--- 404.php
+++ 404.php
@@ -16,9 +16,9 @@ get_header();
 		<main id="main" class="site-main">
 
 			<div class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'No se ha encontrado la página...', 'twentynineteen' ); ?></h1>
-				</header><!-- .page-header -->
+				<!-- <header class="page-header">
+					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentynineteen' ); ?></h1>
+				</header>.page-header -->
 
 				<div class="page-content">
 					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentynineteen' ); ?></p>
