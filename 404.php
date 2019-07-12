7f1b939336d667ed695b36ae5b4a694ba704be21
Marielad <marieladorta@gmail.com>
2019-07-12 10:10:37 +0100

    404

--- 404.php
+++ 404.php
@@ -17,7 +17,7 @@ get_header();
 
 			<div class="error-404 not-found">
 				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentynineteen' ); ?></h1>
+					<h1 class="page-title"><?php _e( 'No se ha encontrado la página...', 'twentynineteen' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
